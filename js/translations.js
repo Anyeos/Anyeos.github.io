2c9dccd17ee7fa566840566597ce533aae12c4e9
Anyeos <anyeos@gmail.com>
2026-06-18 07:17:06 -0300

    Primera versión pública.

---
+++ js/translations.js
@@ -0,0 +1,10 @@
+const resources = {
+  es: { 
+  title_text: "Schwartz Germán Andrés - Mi Currículo",
+  app_description: "Esta es nuestra aplicación.",
+  },
+  en: { 
+  title_text: "Schwartz German Andres - My Curriculum",
+  app_description: "This is our application." },
+};
+

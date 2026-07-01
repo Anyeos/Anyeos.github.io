ef1478bcb0260cb6c6564031fb474537fe0f971e
Anyeos <anyeos@gmail.com>
2026-07-01 08:25:26 -0300

    Fixes

--- js/translations.js
+++ js/translations.js
@@ -1,25 +1,27 @@
 var translations = {};
+const userLang = new Intl.Locale(navigator.language).language;
+const languageSelector = document.getElementById("languageSelector");
 
 async function loadTranslations() {
     try {
         const response = await fetch('translations.json');
         if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
+          languageSelector.remove();
+          throw new Error(`HTTP error! Status: ${response.status}`);
         }
-
         translations = await response.json();
-        //console.log(text);
+
+        languageSelector.value = userLang;
+
+        localjs.init(languageSelector.value, translations);
+        languageSelector.addEventListener("change", function() {
+          localjs.update(languageSelector.value, translations);
+        });
+
     } catch (error) {
-        console.error('Failed to load file:', error);
+      languageSelector.remove();
+      console.error('Failed to load file:', error);
     }
 }
 
 loadTranslations();
-const userLang = new Intl.Locale(navigator.language).language;
-const languageSelector = document.getElementById("languageSelector");
-languageSelector.value = userLang;
-
-localjs.init(languageSelector.value, translations);
-languageSelector.addEventListener("change", function() {
-  localjs.update(languageSelector.value, translations);
-});
